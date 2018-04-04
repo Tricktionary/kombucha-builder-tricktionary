158b085a5eb49bbfda8a06f08694abdcfd6558fb
Patrick Vice <patrickgvice@gmail.com>
2018-04-04 16:16:43 -0400

    added to the readme

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,10 @@
 class ApiController < ApplicationController
 
   protected
+    def current_user
+      @user ||= User.find(session[:current_user_id])
+    end
+
     def authenicate_user!
       session[:current_user_id].present?
     end
