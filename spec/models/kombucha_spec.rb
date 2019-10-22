a5d9fc31e2aee61b666f667ca3e0888d66175d01
Tricktionary <andyaoux@hotmail.com>
2019-10-22 13:16:07 -0400

    Add model test for kombucha

--- spec/models/kombucha_spec.rb
+++ spec/models/kombucha_spec.rb
@@ -3,5 +3,42 @@
 require 'rails_helper'
 
 describe Kombucha, type: :model do
-  fixtures :kombuchas
+  let(:kombucha) { Kombucha.first }
+  let(:current_user) { User.first }
+
+  describe `valid?` do
+    let(:result) { kombucha.valid? }
+
+    it 'valid kombucha' do
+      expect(result).to be(true)
+    end
+
+    it 'base ingredient' do
+      expect(kombucha.base_ingredient.name).to eq("Black Tea")
+    end
+    
+    it 'caffeine free' do
+      expect(kombucha.caffeine_free).to eq(false)
+    end 
+
+    it 'vegan' do
+      expect(kombucha.vegan).to eq(true)
+    end 
+
+    context 'average rating when rating exist' do
+      before do 
+        rating = Rating.create(user: current_user, kombucha: kombucha, rating: 1)
+      end 
+      it 'expect 1' do 
+        expect(kombucha.average_rating).to eq(1)
+      end 
+    end 
+
+    context 'average rating when no rating exist' do
+      it 'expect 5' do 
+        expect(kombucha.average_rating).to eq(5)
+      end 
+    end
+  end 
+
 end
